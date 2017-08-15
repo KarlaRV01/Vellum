7349dffa65aef0af51bea18de99415bc80556ea9
mellifluousbookworm27 <bgutierrez2019@columbiasecondary.org>
2017-08-15 15:51:54 -0400

    added dictionary code

--- Vellum-vellumfiles/websitejs.js
+++ Vellum-vellumfiles/websitejs.js
@@ -1,1 +1,15 @@
-
+function checkPassword() {
+  var password = document.getElementById("pw").value;
+  var isSecure = true;
+  for (word=0; word <=wordsList.length; word++) {
+    if (password==wordsList[word]) {
+        document.getElementById('results').innerHTML = "This is a weak password!";
+      isSecure = false;
+      console.log("Your password is weak");
+    }
+  }
+  if (isSecure == true) {
+    console.log("Your password is strong");
+    document.getElementById('results').innerHTML = "This is a strong password!";
+  }
+}
