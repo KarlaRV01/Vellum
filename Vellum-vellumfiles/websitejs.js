e7d8e84f411b8c25523b5f3394657cf24ea9aaeb
mellifluousbookworm27 <bgutierrez2019@columbiasecondary.org>
2017-08-17 10:30:20 -0400

    put entire dictionary code

--- Vellum-vellumfiles/websitejs.js
+++ Vellum-vellumfiles/websitejs.js
@@ -1,5 +1,20 @@
+var wordsList = [];
+
+function init() {
+  // Load the words from the dictionary text file to wordsList
+  var wordsFile = "https://raw.githubusercontent.com/GirlsFirst/SIP-2017/master/Unit2_Applications/dictionary-attack/dictionary.txt?token=ADcVhZjRMd86ZdhPE2jVvIaJdQdzLA6Yks5YvvVSwA%3D%3D";
+  $.get(wordsFile, function(data) {
+    document.getElementById("btnSubmit").disabled = true;
+    wordsList = data.split('\n');
+    document.getElementById("btnSubmit").disabled = false;
+  });
+}
+
+window.onload = init;
+
 function checkPassword() {
-  var password = document.getElementById("pw").value;
+  var password = document.getElementById("exampleInputPassword").value; //instead of ElementbyID => elementsbyclassname
+
   var isSecure = true;
   for (word=0; word <=wordsList.length; word++) {
     if (password==wordsList[word]) {
